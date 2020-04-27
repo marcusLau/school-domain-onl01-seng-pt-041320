735a1e3664c10dbef9d97a36977c55eb83860e17
Marcus Lau <lau.marcus76@gmail.com>
2020-04-27 22:07:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster 
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster.key?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  # sorts thru each grade alphabetically
+  def sort 
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
